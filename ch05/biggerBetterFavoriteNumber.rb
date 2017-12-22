2c558a2703d36d7f21bdaf02de1003d52a38e588
WuRebecca <Rebecca@WuRebeccas-MacBook-Pro.local>
2017-12-22 14:55:40 +0800

    Add Chapter 5 exercises

---
+++ ch05/biggerBetterFavoriteNumber.rb
@@ -0,0 +1,4 @@
+puts "Hi. What is your favorite number?"
+number = gets.chomp
+better_number = number.to_i + 1
+puts "I have a suggestion. Isn't " + better_number.to_s + " a bigger, better favorite number?"
---
+++ ch05/fullNameGreeting.rb
@@ -0,0 +1,7 @@
+puts "Hello there. What is your first name?"
+first_name = gets.chomp
+puts "Cool, and your middle name?"
+middle_name = gets.chomp
+puts "What about your last name?"
+last_name = gets.chomp
+puts "Pleased to meet you, " + first_name + " " + middle_name + " " + last_name + "."
