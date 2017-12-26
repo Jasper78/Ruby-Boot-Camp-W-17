7b76839486f59ee9b5371c1357112e7c0eec223b
WuRebecca <Rebecca@WuRebeccas-MacBook-Pro.local>
2017-12-26 15:23:16 +0800

    Modify user interface

--- ch06/angryBoss.rb
+++ ch06/angryBoss.rb
@@ -6,8 +6,9 @@
 BEGIN {puts}
 END {puts}
 #Program prompts the user to speak to Angry Boss
-puts "You step into your boss's office."
+puts ">You step into your boss's office."
 puts "Boss: WELL, WHAT DO YOU WANT?"
 whee = gets.chomp.upcase
 #Angry Boss responds
 puts "Boss: WHADDAYA MEAN \"" + whee + "\"?!? YOU'RE FIRED!!"
+puts ">You have been kicked out of your boss's office."
