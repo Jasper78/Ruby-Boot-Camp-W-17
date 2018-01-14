0e086000f657aee3d51ed18e47294baaf6f3d92b
WuRebecca <Rebecca@WuRebeccas-MacBook-Pro.local>
2018-01-14 15:22:38 +0800

    Add Chap 10

--- ch10/shuffle.rb
+++
@@ -1,9 +0,0 @@
-#Author: Rebecca Wu (Jasper78)
-#Date: 12/xx/2017
-#This program
-
-#Jasper78's Personalized Coding Style
-BEGIN {puts}
-END {puts}
-
-# ---------- Defines method to state number of bottles in English -------------
